e9622365bc74b382cd9d9d00eec6999b7ce4b6a6
Bryan Green <twistedogre@gmail.com>
2015-05-10 17:47:02 -0700

    fix

--- lib/gpio_module.ex
+++ lib/gpio_module.ex
@@ -1,7 +1,7 @@
 defmodule GpioModule do
 
 	def start_gpio(name, input_pin, led_pin) do
-		{:ok, pid} = Gpio.start_link(pin, :input)
+		{:ok, pid} = Gpio.start_link(input_pin, :input)
 		:ok = Gpio.set_int(pid, :rising)
 		:ok
 	end
