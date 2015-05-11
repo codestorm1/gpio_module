43d9a059b657babeede6cffde8822861182c613c
Bryan Green <twistedogre@gmail.com>
2015-05-10 17:59:58 -0700

    test

--- test/gpio_module_test.exs
+++ test/gpio_module_test.exs
@@ -11,7 +11,7 @@ defmodule GpioModuleTest do
 
   test "gpio test" do
     assert :ok = GpioModule.start_gpio("channel Z", 17, 5)
-		GpioModule.listen_for_change(1)
+		GpioModule.listen_for_change
   end
 	
 	
