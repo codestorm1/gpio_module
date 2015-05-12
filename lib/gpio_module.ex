1b00ea138fcad2a4b2d93f653d9b262d1d90de6c
Bryan Green <twistedogre@gmail.com>
2015-05-11 21:06:08 -0700

    switching away turns off leds

--- lib/gpio_module.ex
+++ lib/gpio_module.ex
@@ -4,7 +4,7 @@ defmodule GpioModule do
 
 	def start_gpio(name, input_pin, led_pin) do
 		{:ok, pid} = Gpio.start_link(input_pin, :input)
-		:ok = Gpio.set_int(pid, :rising)
+		:ok = Gpio.set_int(pid, :both)
 	end
 
 
@@ -16,14 +16,18 @@ defmodule GpioModule do
 				IO.puts "got rising 17"
 				Gpio.write(pid6, 0)
 				Gpio.write(pid5, 1)
-				#:timer.sleep(1000)
 				listen_for_change(pid5, pid6)
 			{:gpio_interrupt, 18, :rising} -> 
 				IO.puts "got rising 18"
 				Gpio.write(pid5, 0)
 				Gpio.write(pid6, 1)
-				#:timer.sleep(1000)
 				listen_for_change(pid5, pid6)
+				{:gpio_interrupt, 17, :falling} -> 
+					Gpio.write(pid5, 0)
+					listen_for_change(pid5, pid6)
+				{:gpio_interrupt, 18, :rising} -> 
+					Gpio.write(pid6, 0)
+					listen_for_change(pid5, pid6)
 			{:gpio_interrupt, _pin, _state} ->
 				listen_for_change(pid5, pid6)
 		end
