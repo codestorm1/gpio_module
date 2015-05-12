a9b4fd7eaab487a5fd2c7492376ee64e580bbe90
Bryan Green <twistedogre@gmail.com>
2015-05-11 23:54:55 -0700

    closer to generic code

--- lib/gpio_module.ex
+++ lib/gpio_module.ex
@@ -1,35 +1,32 @@
-defmodule GpioModule do
+	defmodule GpioModule do
 
-
-
-	def start_gpio(name, input_pin, led_pin) do
+	def start_gpio(name, input_pin, led_pin, state) do
 		{:ok, pid} = Gpio.start_link(input_pin, :input)
 		:ok = Gpio.set_int(pid, :both)
+		# {:ok, %{state | 17 => %{name: name, led_pin: led_pin}}}
+		{:ok, led_pid} = Gpio.start_link(led_pin, :output)
+		state = Dict.put_new(state, input_pin, %{:name => name, :led_pid => led_pid})
+		{:ok, state}
 	end
 
-
-	def listen_for_change(pid5, pid6) do
-		
+  def listen_for_change(state) do
 		receive do
-#			{:gpio_interrupt, pin, state} -> IO.puts "got something #{pin} #{state}"
-			{:gpio_interrupt, 17, :rising} -> 
-				IO.puts "got rising 17"
-				Gpio.write(pid6, 0)
-				Gpio.write(pid5, 1)
-				listen_for_change(pid5, pid6)
-			{:gpio_interrupt, 18, :rising} -> 
-				IO.puts "got rising 18"
-				Gpio.write(pid5, 0)
-				Gpio.write(pid6, 1)
-				listen_for_change(pid5, pid6)
-				{:gpio_interrupt, 17, :falling} -> 
-					Gpio.write(pid5, 0)
-					listen_for_change(pid5, pid6)
-				{:gpio_interrupt, 18, :rising} -> 
-					Gpio.write(pid6, 0)
-					listen_for_change(pid5, pid6)
+			{:gpio_interrupt, input_pin, :rising} ->
+				IO.puts "got rising #{input_pin}"
+				channel = state[input_pin]
+				IO.inspect channel
+				q = channel[:led_pid]
+				IO.puts "wot?"
+				IO.inspect q
+				Gpio.write(q, 1)
+				listen_for_change(state)
+
 			{:gpio_interrupt, _pin, _state} ->
-				listen_for_change(pid5, pid6)
+				listen_for_change(state)
+			{_, _pin, _state} ->
+				listen_for_change(state)
+			{_, _, _, _} ->
+				:ok
 		end
 	end
 end
--- test/gpio_module_test.exs
+++ test/gpio_module_test.exs
@@ -1,21 +1,14 @@
 defmodule GpioModuleTest do
   use ExUnit.Case
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-
-  test "gpio start test" do
-    assert :ok = GpioModule.start_gpio("channel Z", 17, 5)
-  end
 	
   @tag timeout: 300000
   test "gpio test" do
-    assert :ok = GpioModule.start_gpio("channel Z", 17, 5)
-    assert :ok = GpioModule.start_gpio("channel Z", 18, 4)
-	  {:ok, pid5} = Gpio.start_link(5, :output)
-	  {:ok, pid6} = Gpio.start_link(6, :output)
-		GpioModule.listen_for_change(pid5, pid6)
+		state = %{}
+    assert {:ok, state} = GpioModule.start_gpio("channel blue", 17, 5, state)
+    assert {:ok, state} = GpioModule.start_gpio("channel red", 18, 4, state)
+#	  {:ok, pid5} = Gpio.start_link(5, :output)
+#	  {:ok, pid6} = Gpio.start_link(6, :output)
+		GpioModule.listen_for_change(state)
   end
 	
 	
